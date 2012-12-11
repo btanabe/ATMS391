ed0889e47cd35b435e6fb09c18fe29a67d9cbe03
Brian Tanabe <tanabesan4@gmail.com>
2012-12-10 21:54:51 -0600

    Added day values to EnergyTab.

--- src/com/atms391/android/gui/tabs/EnergyTabFragment.java
+++ src/com/atms391/android/gui/tabs/EnergyTabFragment.java
@@ -35,8 +35,22 @@ public class EnergyTabFragment extends Fragment {
 
 		TextView hoursOfRefridgeratorOperationTextView = (TextView) getActivity().findViewById(R.id.energyTabHeaderTextView);
 		if(hoursOfRefridgeratorOperationTextView != null){
-			String hoursOfRefridgeratorOperation = String.valueOf(NumberPrinterHelper.roundToTwoDecimalPlaces(hours)) + " hours.";
-			hoursOfRefridgeratorOperationTextView.setText("Your solar panel array would produce enough energy to power a 725W referigerator for " + hoursOfRefridgeratorOperation);
+			double hoursOfOperation = NumberPrinterHelper.roundToTwoDecimalPlaces(hours);
+			double daysOfOperation = NumberPrinterHelper.roundToTwoDecimalPlaces(hours / 24.00);
+			
+			StringBuilder message = new StringBuilder();
+			message.append("Your solar panel array would produce enough energy in one month to power a 725W refrigerator for ");
+			message.append(hoursOfOperation);
+			message.append(" hours");
+			if(daysOfOperation > 1){
+				message.append(" or ");
+				message.append(daysOfOperation);
+				message.append(" days.");
+			} else {
+				message.append(".");
+			}
+			
+			hoursOfRefridgeratorOperationTextView.setText(message.toString());
 		}
 	}
 	
